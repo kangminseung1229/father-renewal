3045aadd9f279fc1bff46b4c72f32c0cc5f01985
kangminseung <whelming25@naver.com>
2023-07-01 11:22:22 +0900

    init

---
+++ src/main/kotlin/kr/co/inaus/mskfather/MskFatherApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.inaus.mskfather
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MskFatherApplication
+
+fun main(args: Array<String>) {
+    runApplication<MskFatherApplication>(*args)
+}
